85e323207b371bf2bca5956f38baf02420060a97
Dmyto_Turovskiy <turovskydo@gmail.com>
2022-01-21 14:37:57 +0200

    Section products done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
 
+    $('ul.products__tabs').on('click', 'li:not(.products__tab_active)', function() {
+    $(this)
+      .addClass('products__tab_active').siblings().removeClass('products__tab_active')
+      .closest('div.container').find('div.products__content').removeClass('products__content_active').eq($(this).index()).addClass('products__content_active');
+    });
+
 });
